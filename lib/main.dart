8de72121b1b53a9adda41ab874289e10c7c08424
gonzaloac <gonzaloac24@gmail.com>
2023-11-09 00:50:26 -0500

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,334 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mi Juego de Desafíos',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String nombre = "";
 
-  void _incrementCounter() {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Descubrelo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Bienvenido a Descubrelo',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Ingresa tu nombre',
+              ),
+              onChanged: (value) {
+                setState(() {
+                  nombre = value;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                if (nombre.isNotEmpty) {
+                  // Navegar a la siguiente pantalla con el nombre del usuario
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MainScreen(nombre: nombre),
+                    ),
+                  );
+                } else {
+                  // Mostrar un mensaje de error si el nombre está vacío
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('Error'),
+                        content: Text('Por favor, ingresa tu nombre.'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('OK'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+              },
+              child: Text('Continuar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  final String nombre;
+
+  MainScreen({required this.nombre});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('¡Hola, $nombre!'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hola $nombre, ¿dónde te encuentras?',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navega a la tercera pantalla cuando se presiona el botón "Bar"
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CategoriaScreen(nombre: nombre),
+                  ),
+                );
+              },
+              child: Text('Bar'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Puedes agregar la lógica para el botón de "Casa" aquí.
+              },
+              child: Text('Casa'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Puedes agregar la lógica para el botón de "Disco" aquí.
+              },
+              child: Text('Disco'),
+            ),
+            // Puedes agregar más botones según sea necesario para otros lugares.
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+String preguntaAleatoria(String categoria) {
+  final Map<String, List<String>> preguntasPorCategoria = {
+    'Picante': [
+      'Dile a la persona más atractiva de la habitación que te gusta.',
+      'Besa a alguien del mismo sexo.',
+      // Agrega más preguntas picantes aquí.
+    ],
+    'Caliente': [
+      'Baila de manera sensual con alguien.',
+      'Dibuja un corazón en tu brazo con lápiz labial.',
+      // Agrega más preguntas "calientes" aquí.
+    ],
+    'Descúbrelo': [
+      'Elige un número entre 1 y 10 y cuenta un secreto sobre ti por cada número.',
+      'Haz una pregunta indiscreta a alguien y responde tú también.',
+      // Agrega más preguntas "descúbrelo" aquí.
+    ],
+  };
+
+  final List<String> preguntas = preguntasPorCategoria[categoria]!;
+  if (preguntas != null && preguntas.isNotEmpty) {
+    final random = Random();
+    final indice = random.nextInt(preguntas.length);
+    return preguntas[indice];
+  }
+  return 'No hay preguntas disponibles para esta categoría.';
+}
+
+
+class CategoriaScreen extends StatelessWidget {
+  final String nombre;
+
+  CategoriaScreen({required this.nombre});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('¡Hola, $nombre!'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '¿Qué categoría eliges?',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                final categoriaElegida = 'Picante'; // Debes obtener la categoría seleccionada aquí
+                final pregunta = preguntaAleatoria(categoriaElegida);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => PreguntaScreen(pregunta: pregunta),
+                  ),
+                );
+              },
+              child: Text('Picante'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Lógica para la categoría "Caliente"
+                final categoriaElegida = 'Caliente'; // Debes obtener la categoría seleccionada aquí
+                final pregunta = preguntaAleatoria(categoriaElegida);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => PreguntaScreen(pregunta: pregunta),
+                  ),
+                );
+              },
+              child: Text('Caliente'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Lógica para la categoría "Descúbrelo"
+                final categoriaElegida = 'Descúbrelo'; // Debes obtener la categoría seleccionada aquí
+                final pregunta = preguntaAleatoria(categoriaElegida);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => PreguntaScreen(pregunta: pregunta),
+                  ),
+                );
+              },
+              child: Text('Descúbrelo'),
+            ),
+            // Puedes agregar más botones para otras categorías.
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PreguntaScreen extends StatefulWidget {
+  final String pregunta;
+
+  PreguntaScreen({required this.pregunta});
+
+  @override
+  _PreguntaScreenState createState() => _PreguntaScreenState();
+}
+
+class _PreguntaScreenState extends State<PreguntaScreen> {
+  String preguntaActual = '';
+
+  @override
+  void initState() {
+    super.initState();
+    preguntaActual = widget.pregunta;
+  }
+
+  String preguntaAleatoria(String categoria) {
+    final Map<String, List<String>> preguntasPorCategoria = {
+      'Picante': [
+        'Dile a la persona más atractiva de la habitación que te gusta.',
+        'Besa a alguien del mismo sexo.',
+        // Agrega más preguntas picantes aquí.
+      ],
+      'Caliente': [
+        'Baila de manera sensual con alguien.',
+        'Dibuja un corazón en tu brazo con lápiz labial.',
+        // Agrega más preguntas "calientes" aquí.
+      ],
+      'Descúbrelo': [
+        'Elige un número entre 1 y 10 y cuenta un secreto sobre ti por cada número.',
+        'Haz una pregunta indiscreta a alguien y responde tú también.',
+        // Agrega más preguntas "descúbrelo" aquí.
+      ],
+    };
+
+    final List<String>? preguntas = preguntasPorCategoria[categoria];
+    if (preguntas != null && preguntas.isNotEmpty) {
+      final random = Random();
+      final indice = random.nextInt(preguntas.length);
+      return preguntas[indice];
+    }
+    return 'No hay preguntas disponibles para esta categoría.';
+  }
+
+  void cambiarPregunta() {
+    final categoriaElegida = 'Picante'; // Debes obtener la categoría seleccionada aquí
+    final pregunta = preguntaAleatoria(categoriaElegida);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      preguntaActual = pregunta;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Pregunta'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFFE63130), Color(0xFFFA8623)],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                preguntaActual,
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  cambiarPregunta();
+                },
+                child: Text('Volver a Pregunta'),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
